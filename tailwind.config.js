0b122d7574eb55ce7fd8b754922cc95a6c3e7805
Mailar <veex1233@gmail.com>
2021-12-01 18:34:53 +0200

    Installed tailwind with node

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [
+    './public/*.html'
+
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+
+    },
+  },
+  variants: {
+    extend: {
+
+    },
+  },
+  plugins: [
+
+  ],
+}
