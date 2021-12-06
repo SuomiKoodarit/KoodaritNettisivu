7e9a5bb1274c22f9de82ab687c97fccd890b2377
Mailar <veex1233@gmail.com>
2021-12-06 17:03:56 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ function christmasTheme() {
     let d = new Date();
     n = d.getMonth();
 
-    if (n == 11) {
+    if (n === 11) {
         var changeLogo = document.getElementById("logo")
         changeLogo.src = "/assets/imgs/logo.png";
     }
